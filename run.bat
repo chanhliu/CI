cf7c1a6bd4e2d5cb320ec95743bc8b824c09869c
Chanh Liu <chanh.liu@opswat.com>
2021-09-03 15:10:00 +0700

    First commit

---
+++ run.bat
@@ -0,0 +1,3 @@
+cd D:\OPSWAT\Work\software\eclipse-workspace\CD
+CALL mvn clean test
+cmd /k
---
+++ src/test/java/com/test/basicTestCase.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import org.testng.annotations.Test;
+
+public class basicTestCase {
+	
+	
+	@Test
+	public void test1() {
+		System.out.println("This is the first test case");
+	}
+}
